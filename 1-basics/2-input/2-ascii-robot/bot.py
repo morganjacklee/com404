f9554159d7f84232ccf8f69c2385228f1809345f
Morgan Lee <hello@morganlee.me>
2018-09-27 17:20:06 +0100

    Create bot.py
    Added code to display a dynamic ascii robot.

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,16 @@
+# Ask user to enter character
+print("Please enter a character for the eyes")
+eye = input()
+
+
+# Display ascii art with variable defined by user
+print("\t   #####")
+print("\t  @@@@@@@")
+print("\t ooooooooo")
+print("        |    o    |")
+print("        ( ",eye,eye," o ",eye,eye," )", sep="")
+print("        |    o    |")
+print("\t ooooooooo")
+print("\t ooooooooo")
+print("\t oo     oo")
+print("        oo       oo")
