df41cd671ba79a7e2cb5cda4cfc1be270cbc1d02
Morgan Lee <morganjacklee@outlook.com>
2018-09-27 15:55:14 +0100

    Create bot.py
    Added some code to demonstrate the use of multiple escape characters.

---
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -0,0 +1,8 @@
+# Display escape characters
+print("\n Display a new line.")
+print("\t Display a tab space.")
+print("\\ Display a back slash.")
+print("\"Display a double quote.\"")
+
+#Testing using multiple escape characters
+print("\n\t\"May the force be with you.\"")
