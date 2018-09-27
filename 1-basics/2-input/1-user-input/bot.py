9e6ef4858b2bf386cd284204e1f97cab5dc189fb
Morgan Lee <hello@morganlee.me>
2018-09-27 17:02:19 +0100

    Update bot.py
    Added code to override default separator.

--- 1-basics/2-input/1-user-input/bot.py
+++ 1-basics/2-input/1-user-input/bot.py
@@ -2,3 +2,7 @@
 print("Please enter your name")
 name = input()
 print("Your name is",name+".")
+
+# Alternatively, we can do the following:
+print("Your name is ",name,".", sep="")
+# This overrides the seperater to remove any spaces
