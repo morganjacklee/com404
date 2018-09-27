e46e11033e45e989e8bcbfe8018d693689919715
Morgan Lee <hello@morganlee.me>
2018-09-27 17:51:57 +0100

    Create bot.py
    Added code to demonstrate the use of multiplication operator with strings.

---
+++ 1-basics/2-input/4-string-operator/bot.py
@@ -0,0 +1,12 @@
+# Display energy bars
+print("Enter the number of lives remaining:")
+lives = int(input())
+print("Enter energy level:")
+energy = int(input())
+print("Enter shield level:")
+shield = int(input())
+
+
+print("Lives: ","♡" * lives)
+print("Energy: ","☼"* energy)
+print("Shield: ","◆"* shield)
