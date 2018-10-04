ae1294cc9d80c65b7a8ac8075aa54b6bc0e10c60
Morgan Lee <hello@morganlee.me>
2018-10-04 17:20:02 +0100

    Create bot.py
    Added multiple nested else/if/elif statements.

---
+++ 1-basics/3-decision/8-nestception/bot.py
@@ -0,0 +1,40 @@
+print("Where shall I look?")
+print("Choices are:")
+print("""- Bedroom
+- Bathroom
+- Labratory
+- Somewhere else""")
+location = str(input())
+
+# Using if, else and elif statements
+if location == "Bedroom" :
+  print("Where in the bedroom?")
+  print("""- Under the bed
+- Somewhere else""")
+  location_bedroom = str(input())
+  if location_bedroom == "Under the bed" :
+    print("Found some socks but no battery.")
+  elif location_bedroom == "Somewhere else" :
+    print("Found some mess but no battery.")
+elif location == "Bathroom":
+  print("Where in the bathroom?")
+  print("""- In the Bathtub
+- Somewhere else""")
+  location_bathroom = str(input())
+  if location_bathroom == "In the bathtub" :
+    print("Found a rubber duck but no battery.")
+  elif location_bathroom == "Somewhere else" :
+    print("It's wet and there is no battery.")
+elif location == "Labratory":
+  print("Where in the labratory?")
+  print("""- On the table
+- Somewhere else""")
+  location_labratory = str(input())
+  if location_labratory == "On the table" :
+    print("Found the battery!")
+  elif location_labratory == "Somewhere else" :
+    print("Found some tools but no battery.")
+elif location == "Somewhere else":
+  print("Found some mess but no battery.")
+else:
+  print("I don't know where that is but I will keep looking!")
