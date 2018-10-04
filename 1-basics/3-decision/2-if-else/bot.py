31777f829da2ce1b3567b4b022287e7a7ea277eb
Morgan Lee <hello@morganlee.me>
2018-10-04 15:44:04 +0100

    Create bot.py
    Added code to do an if and else statement.

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,13 @@
+# Read direction from user
+print("Please enter one of the following directions")
+print("W is to go up.")
+print("S is to go down.")
+print("A is to go left.")
+print("D is to go right.")
+direction = str(input())
+
+# Using if, else and elif statements
+if direction == "w":
+  print("You are moving up.")
+else :
+  print ("I don't know where you are going!")
