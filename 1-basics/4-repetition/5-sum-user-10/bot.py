e6da55771e46c0fa4c3b44527bf4128655f2f459
Morgan Lee <hello@morganlee.me>
2018-10-10 16:59:53 +0100

    Create bot.py
    Added code to demonstrate how to sum the first 10 numbers from the user.

---
+++ 1-basics/4-repetition/5-sum-user-10/bot.py
@@ -0,0 +1,12 @@
+COUNT = 10
+TOTAL = 0
+
+
+while (COUNT > 0):
+  print("Enter a number")
+  number = int(input())
+  COUNT = COUNT - 1
+  TOTAL = TOTAL + number
+
+  
+print ("The overall amount is",TOTAL)	
