aa833ff85b4e0e659f1bdd555fa10f57340ef116
Morgan Lee <hello@morganlee.me>
2018-10-17 18:07:53 +0100

    Create bot.py
    Added code to manipulate words with user-defined functions.

---
+++ 1-basics/4-repetition/9-word-manipulation/bot.py
@@ -0,0 +1,56 @@
+def display_box(text):
+  print("*" * (len(text) + 10))
+  print("*", text, "*")
+  print("*" * (len(text) + 10))
+
+def display_lower(text):
+  print(text.lower())
+
+def display_upper(text):
+  print(text.upper())
+
+def display_mirrored(text):
+  for position in range(len(text)-1, -1, -1):
+	  print(text[position], end="")
+
+
+def display_repeat(text):
+  print("How many times?")
+  number = str(input())
+
+  for count in range(0,number, 1):
+    print(text)
+
+def greet_user():
+    print("Please enter a word to manipulate:")
+    text = str(input())
+
+    pick = True
+    while pick == True:
+      print("Please pick from the following: (1-5)")
+      print("""1) Display in a Box – display the word in an ascii art box
+2) Display Lower-case – display the word in lower-case e.g. hello
+3) Display Upper-case – display the word in upper-case e.g. HELLO
+4) Display Mirrored – display the word with its mirrored word e.g. Hello | olleH
+5) Repeat – how many times to display the word and then display the word that many times alternating between upper-case and lower-case.""")
+      choice = str(input())
+      if choice == "1":
+        pick = False
+        display_box(text)
+      elif choice == "2":
+        pick = False
+        display_lower(text)
+      elif choice == "3":
+        pick = False
+        display_upper(text)
+      elif choice == "4":
+        pick = False
+        display_mirrored(text)
+      elif choice == "5":
+        pick = False
+        display_repeat(text)
+  
+
+  
+  
+greet_user()
