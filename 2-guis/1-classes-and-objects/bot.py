c75215262ec6cdccb2e02c46f7bdc84bb36f5a0d
Morgan Lee <hello@morganlee.me>
2018-10-31 18:43:43 +0000

    Update bot.py
    Added appropriate access modifiers.

--- 2-guis/1-classes-and-objects/bot.py
+++ 2-guis/1-classes-and-objects/bot.py
@@ -1,57 +1,57 @@
 class Bot:
   
   def __init__(self, name, age=0, energy=100, shield_level=100):
-    self.name = name
-    self.age = age
-    self.energy = energy
-    self.shield_level = shield_level
+    self.__name = name
+    self.__age = age
+    self.__energy = energy
+    self.__shield_level = shield_level
 
 # First set
   def get_age(self):
-    return self.age
+    return self.__age
   def get_energy(self):
-    return self.energy
+    return self.__energy
   def get_name(self):
-    return self.name
+    return self.__name
   def get_shield(self):
-    return self.shield_level
+    return self.__shield_level
 
 # Second set
   def decrement_energy(self, amount):
-    self.energy = (self.energy - amount)
+    self.__energy = (self.__energy - amount)
 
   def decrement_shield(self, amount):
-    self.shield = (self.shield - amount)
+    self.__shield_level = (self.__shield_level - amount)
 
 # Third set
   def display_age(self):
-    print("{} is the age of the bot".format(self.age))
+    print("{} is the age of the bot".format(self.__age))
 
   def display_energy(self):
-    print("Currently {} has the energy of {}.".format(self.name,self.energy))
+    print("Currently {} has the energy of {}.".format(self.__name,self.__energy))
 
   def display_name(self):
-    print("{} is the name of the bot".format(self.name))
+    print("{} is the name of the bot".format(self.__name))
 
   def display_shield(self):
-    print("The shield levels of {} are currently {}.".format(self.name,self.shield_level))
+    print("The shield levels of {} are currently {}.".format(self.__name,self.__shield_level))
 
   def display_summary(self):
-    print("{} is currently {} years old, and has the level of {} energy. The shields are currently set as {}.".format(self.name,self.age,self.energy,self.shield_level))
+    print("{} is currently {} years old, and has the level of {} energy. The shields are currently set as {}.".format(self.__name,self.__age,self.__energy,self.__shield_level))
 
   def __str__(self):
-    return("{} is currently {} years old, and has the level of {} energy. The shields are currently set as {}.".format(self.name,self.age,self.energy,self.shield_level))
+    return("{} is currently {} years old, and has the level of {} energy. The shields are currently set as {}.".format(self.__name,self.__age,self.__energy,self.__shield_level))
 
 # Fourth set
   def increment_age(self, amount):
-    self.age = (self.age + amount)
+    self.__age = (self.__age + amount)
 
   def increment_energy(self, amount):
-    self.energy = (self.energy + amount)
+    self.__energy = (self.__energy + amount)
 
   def increment_shield(self, amount):
-    self.shield = (self.shield + amount)
+    self.__shield_level = (self.__shield_level + amount)
 
 # Fifth set
   def set_name(self, name):
-    self.name = name
+    self.__name = name
